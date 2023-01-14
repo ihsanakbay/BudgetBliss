ff0a4aaec8e3128724032386ddfdbd595666779d
Ihsan Akbay <ihsanakbay0@gmail.com>
2023-01-15 00:09:32 +0300

    Initial Commit

---
+++ BudgetBliss/BudgetBlissApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetBlissApp.swift
+//  BudgetBliss
+//
+//  Created by İhsan Akbay on 15.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct BudgetBlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
